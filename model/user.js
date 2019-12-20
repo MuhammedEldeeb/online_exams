0eaea61931dd16b4974b3ca3d6e81a30ad55ea88
ayheikal <heikal1998@gmail.com>
2019-12-20 18:01:57 +0200

    signup module

---
+++ model/user.js
@@ -0,0 +1,13 @@
+class User{
+    constructor(userId,name,email,password,cvLink,postionId,isApproved)
+    {
+        this.userId=userId;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.cvLink=cvLink;
+        this.postionId=postionId;
+        this.isApproved=isApproved;
+    }
+    
+}
